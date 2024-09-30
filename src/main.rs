ce549bf5792967b1cf4e08456cba43398a666a32
onlyxuyang <onlyxuyang@foxmail.com>
2024-10-01 00:20:44 +0800

    optimize multi-thread implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use glob::glob;
+use glob::GlobError;
+use glob::Pattern;
+use path_slash::PathExt;
 use std::collections::HashMap;
 use std::collections::LinkedList;
 use std::env;
@@ -11,22 +15,22 @@ use std::io::Write;
 use std::os::windows::fs::MetadataExt;
 use std::path::Path;
 use std::path::PathBuf;
-use glob::glob;
-use glob::GlobError;
-use glob::Pattern;
-use path_slash::PathExt;
 use threadpool::ThreadPool;
+use threadpool_scope::scope_with;
 use walkdir::{DirEntry, WalkDir};
 use zip::result::ZipError;
 use zip::write::SimpleFileOptions;
 
-use winapi::um::winuser::{MessageBoxW, MB_OK};
-use std::ptr::null_mut;
 use std::ffi::OsStr;
 use std::os::windows::ffi::OsStrExt;
+use std::ptr::null_mut;
+use winapi::um::winuser::{MessageBoxW, MB_OK, MB_SYSTEMMODAL};
 
 fn to_wide_string(s: &str) -> Vec<u16> {
-    OsStr::new(s).encode_wide().chain(std::iter::once(0)).collect()
+    OsStr::new(s)
+        .encode_wide()
+        .chain(std::iter::once(0))
+        .collect()
 }
 
 fn show_message_box(title: &str, message: &str) {
@@ -34,13 +38,21 @@ fn show_message_box(title: &str, message: &str) {
     let message_wide = to_wide_string(message);
 
     unsafe {
-        MessageBoxW(null_mut(), message_wide.as_ptr(), title_wide.as_ptr(), MB_OK);
+        MessageBoxW(
+            null_mut(),
+            message_wide.as_ptr(),
+            title_wide.as_ptr(),
+            MB_OK | MB_SYSTEMMODAL,
+        );
     }
 }
 
-fn file_op(is_copy:bool, src:&PathBuf, dst:&PathBuf) {
+fn file_op(is_copy: bool, src: &PathBuf, dst: &PathBuf) {
     if !fs::exists(src).unwrap() {
-        println!("File {} is not exists, ignore it", src.as_os_str().to_str().unwrap());
+        println!(
+            "File {} is not exists, ignore it",
+            src.as_os_str().to_str().unwrap()
+        );
         return;
     }
     fs::create_dir_all(dst.parent().unwrap()).unwrap();
@@ -53,29 +65,28 @@ fn file_op(is_copy:bool, src:&PathBuf, dst:&PathBuf) {
     }
 }
 
-fn generate_duplicate_var_files(src_folder:&String) -> Result<HashMap<String, LinkedList<PathBuf>>, GlobError> {
-    let mut result:HashMap<String, LinkedList<PathBuf>> = HashMap::new();
-    for entry in glob(format!("{}/**/*.var", Pattern::escape(src_folder)).as_str()).expect("Failed to read glob pattern") {
+fn generate_duplicate_var_files(
+    src_folder: &String,
+) -> Result<HashMap<String, LinkedList<PathBuf>>, GlobError> {
+    let mut result: HashMap<String, LinkedList<PathBuf>> = HashMap::new();
+    for entry in glob(format!("{}/**/*.var", Pattern::escape(src_folder)).as_str())
+        .expect("Failed to read glob pattern")
+    {
         match entry {
             Ok(path) => {
-                // if !check_vaild_var_file(&path) {
-                //     println!("File {} is not exists, remove it", path.as_os_str().to_str().unwrap());
-                //     fs::remove_file(&path).unwrap();
-                //     continue;
-                // }
                 let filename = String::from(path.file_name().unwrap().to_str().unwrap());
                 if !result.contains_key(&filename) {
                     result.insert(filename.clone(), LinkedList::new());
                 }
                 result.get_mut(&filename).unwrap().push_back(path);
-            },
+            }
             Err(e) => return Err(e),
         }
     }
     Ok(result)
 }
 
-fn get_short_path(path:&PathBuf, base:&PathBuf) -> PathBuf {
+fn get_short_path(path: &PathBuf, base: &PathBuf) -> PathBuf {
     return PathBuf::from_iter(path.strip_prefix(base).unwrap().components().skip(1));
 }
 
@@ -122,7 +133,11 @@ where
     Ok(())
 }
 
-fn zip_one_file(src_dir: &Path, dst_file: &Path, method: zip::CompressionMethod) -> anyhow::Result<()> {
+fn zip_one_file(
+    src_dir: &Path,
+    dst_file: &Path,
+    method: zip::CompressionMethod,
+) -> anyhow::Result<()> {
     if !Path::new(src_dir).is_dir() {
         return Err(ZipError::FileNotFound.into());
     }
@@ -138,9 +153,12 @@ fn zip_one_file(src_dir: &Path, dst_file: &Path, method: zip::CompressionMethod)
     Ok(())
 }
 
-fn rezip_one_file(src:&PathBuf, target:&PathBuf) {
-    let mut result:HashMap<String, (PathBuf,u64)> = HashMap::new();
-    let pattern = format!("{}/**/*", Pattern::escape(src.as_os_str().to_str().unwrap()));
+fn rezip_one_file(src: &PathBuf, target: &PathBuf) {
+    let mut result: HashMap<String, (PathBuf, u64)> = HashMap::new();
+    let pattern = format!(
+        "{}/**/*",
+        Pattern::escape(src.as_os_str().to_str().unwrap())
+    );
     for entry in glob(&pattern).expect("Failed to read glob pattern") {
         match entry {
             Ok(path) => {
@@ -150,32 +168,38 @@ fn rezip_one_file(src:&PathBuf, target:&PathBuf) {
                 let short_name = get_short_path(&path, src);
                 let short_name_str = short_name.as_os_str().to_str().unwrap().to_string();
                 if !result.contains_key(short_name.as_os_str().to_str().unwrap()) {
-                    result.insert(short_name_str, (path.clone(), fs::metadata(&path).unwrap().file_size()));
+                    result.insert(
+                        short_name_str,
+                        (path.clone(), fs::metadata(&path).unwrap().file_size()),
+                    );
                 } else {
                     let size = fs::metadata(&path).unwrap().file_size();
                     if result.get(&short_name_str).unwrap().1 < size {
                         *result.get_mut(&short_name_str).unwrap() = (path.clone(), size);
                     }
                 }
-            },
-            Err(_) => panic!()
+            }
+            Err(_) => panic!(),
         }
     }
     let workdir = src.join("working");
-    for (short_name, (path,_)) in result.iter() {
+    for (short_name, (path, _)) in result.iter() {
         let filepath = workdir.join(short_name);
         file_op(false, path, &filepath);
     }
     zip_one_file(&workdir, target, zip::CompressionMethod::Stored).unwrap();
 }
 
-fn unzip_one_file(path:&PathBuf, base:&PathBuf, idx:usize) {
-    let mut archive = match zip::ZipArchive::new(fs::File::open(path).expect(format!("Could not open file {}", path.as_os_str().to_str().unwrap()).as_str())) {
+fn unzip_one_file(path: &PathBuf, base: &PathBuf, idx: usize) {
+    let mut archive = match zip::ZipArchive::new(
+        fs::File::open(path)
+            .expect(format!("Could not open file {}", path.as_os_str().to_str().unwrap()).as_str()),
+    ) {
         Ok(ret) => ret,
         Err(_) => {
             println!("zipfile {} is invaild", path.as_os_str().to_str().unwrap());
-            return
-        },
+            return;
+        }
     };
 
     for i in 0..archive.len() {
@@ -184,7 +208,7 @@ fn unzip_one_file(path:&PathBuf, base:&PathBuf, idx:usize) {
             Err(_) => {
                 println!("file error, ignore");
                 continue;
-            },
+            }
         };
         let outpath = match file.enclosed_name() {
             Some(path) => path,
@@ -214,10 +238,10 @@ fn main() {
         return;
     }
     let vam_folder = env::current_dir().unwrap();
-    let var_folder = vam_folder.join("AddonPackages");
-    let var_merged_folder = PathBuf::from(&var_folder).join("merged");
-    let var_backup_folder = PathBuf::from(&vam_folder).join("VarCleaner/Backup");
-    let dst_tmp_folder = PathBuf::from(&vam_folder).join("VarCleaner/Tmp");
+    let var_folder = &vam_folder.join("AddonPackages");
+    let var_merged_folder = &PathBuf::from(&var_folder).join("merged");
+    let var_backup_folder = &PathBuf::from(&vam_folder).join("VarCleaner/Backup");
+    let dst_tmp_folder = &PathBuf::from(&vam_folder).join("VarCleaner/Tmp");
     let var_folder_str = var_folder.to_string_lossy();
     let var_merged_folder_str = var_merged_folder.to_string_lossy();
     let var_backup_folder_str = var_backup_folder.to_string_lossy();
@@ -226,40 +250,37 @@ fn main() {
 
     let hpool = ThreadPool::new(12);
     let file_dicts = generate_duplicate_var_files(&var_folder_str.to_string()).unwrap();
-    for (filename, filelist) in file_dicts.iter() {
-        let dst_tmp_folder_clone = dst_tmp_folder.clone();
-        let dst_merged_folder_clone = var_merged_folder.clone();
-        let dst_backup_folder_clone = var_backup_folder.clone();
-        let filename_clone = filename.clone();
-        let poolsize = filelist.len();
-        let src_folder_clone = var_folder.clone();
-        let filelist_clone = filelist.clone();
-        hpool.execute(move || {
-            if filelist_clone.len() > 1 {
-                println!("Process file {} Count {}", filename_clone, filelist_clone.len());
-                let pool = ThreadPool::new(poolsize);
-                let var_tmp_folder = dst_tmp_folder_clone.join(PathBuf::from(&filename_clone));
-                let dst_backup_folder_clone_clone = dst_backup_folder_clone.clone();
-                let src_folder_clone_clone = src_folder_clone.clone();
-                for (pos,item) in filelist_clone.iter().enumerate() {
-                    let item_clone = item.clone();
-                    let var_tmp_folder_clone = var_tmp_folder.clone();
-                    let src_folder_clone_clone_clone = src_folder_clone_clone.clone();
-                    let relative_path = item.strip_prefix(src_folder_clone_clone_clone).unwrap();
-                    let backup_var_path = dst_backup_folder_clone_clone.join(relative_path);
-                    pool.execute(move || {
-                        unzip_one_file(&item_clone,&var_tmp_folder_clone, pos);
-                        create_dir_all(backup_var_path.parent().unwrap()).unwrap();
-                        file_op(false, &item_clone, &backup_var_path);
+    scope_with(&hpool, |hscope| {
+        for (filename, filelist) in file_dicts.iter() {
+            let filename_clone = filename.clone();
+            let filelist_clone = filelist.clone();
+            hscope.execute(move || {
+                if filelist_clone.len() > 1 {
+                    println!(
+                        "Process file {} Count {}",
+                        filename_clone,
+                        filelist_clone.len()
+                    );
+                    let pool = ThreadPool::new(filelist_clone.len());
+                    let var_tmp_folder = &dst_tmp_folder.join(PathBuf::from(&filename_clone));
+                    scope_with(&pool, |scope| {
+                        for (pos, item) in filelist_clone.iter().enumerate() {
+                            let item_clone = item.clone();
+                            scope.execute(move || {
+                                let relative_path = item_clone.strip_prefix(var_folder).unwrap();
+                                let backup_var_path = var_backup_folder.join(relative_path);
+                                unzip_one_file(&item_clone, &var_tmp_folder, pos);
+                                create_dir_all(backup_var_path.parent().unwrap()).unwrap();
+                                file_op(false, &item_clone, &backup_var_path);
+                            });
+                        }
                     });
+                    rezip_one_file(&var_tmp_folder, &var_merged_folder.join(&filename_clone));
+                    fs::remove_dir_all(&var_tmp_folder).unwrap();
                 }
-                pool.join();
-                rezip_one_file(&var_tmp_folder, &dst_merged_folder_clone.join(&filename_clone));
-                fs::remove_dir_all(&var_tmp_folder).unwrap();
-            }
-        });
-    }
-    hpool.join();
+            });
+        }
+    });
     if fs::exists(&dst_tmp_folder).unwrap() {
         fs::remove_dir(&dst_tmp_folder).unwrap();
     }
